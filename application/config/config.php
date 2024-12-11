fc1623be7577c4ba5ef6d1808e8ada0fc7118dc1
Wan Atiq <wanwahid82@gmail.com>
2024-12-11 16:29:26 +0800

    change2

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $path = "http://" . $_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$config['base_url'] = 'https://nadicom.my';
+$config['base_url'] = 'https://nadicom.my/expiredSystem';
 
 /*
 |--------------------------------------------------------------------------
